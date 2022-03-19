efc05043c94486fce00972d61fa8bc4793772dc7
ksalarang <anbleidd@gmail.com>
2022-03-19 21:37:55 +0600

    setup MainActivity and the first fragment

--- app/src/main/java/com/diyartaikenov/app/pricecomparator/MainActivity.kt
+++ app/src/main/java/com/diyartaikenov/app/pricecomparator/MainActivity.kt
@@ -3,9 +3,16 @@ package com.diyartaikenov.app.pricecomparator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import com.diyartaikenov.app.pricecomparator.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
     }
 }
--- app/src/main/java/com/diyartaikenov/app/pricecomparator/ProductListFragment.kt
+++ app/src/main/java/com/diyartaikenov/app/pricecomparator/ProductListFragment.kt
@@ -1,4 +1,35 @@
 package com.diyartaikenov.app.pricecomparator
 
-class ProductListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.diyartaikenov.app.pricecomparator.databinding.FragmentProductListBinding
+
+class ProductListFragment: Fragment() {
+
+    private var _bind: FragmentProductListBinding? = null
+    private val bind get() = _bind!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _bind = FragmentProductListBinding.inflate(inflater, container, false)
+        return bind.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        _bind = null
+        super.onDestroyView()
+    }
 }
