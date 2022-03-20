d42249c694766a89ee18e1ba474a148cfa37edc2
ksalarang <anbleidd@gmail.com>
2022-03-20 11:23:34 +0600

    implement nav graph

--- app/src/main/java/com/diyartaikenov/app/pricecomparator/ui/ProductListFragment.kt
+++ app/src/main/java/com/diyartaikenov/app/pricecomparator/ui/ProductListFragment.kt
@@ -6,8 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.diyartaikenov.app.pricecomparator.BaseApplication
 
+import com.diyartaikenov.app.pricecomparator.BaseApplication
 import com.diyartaikenov.app.pricecomparator.databinding.FragmentProductListBinding
 import com.diyartaikenov.app.pricecomparator.ui.adapter.ProductListAdapter
 
