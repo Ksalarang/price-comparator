3a0819626e26d42c8acba5f807458904c165c645
ksalarang <anbleidd@gmail.com>
2022-03-25 01:33:00 +0600

    add action buttons menu to the ProductListFragment

--- app/src/main/java/com/diyartaikenov/app/pricecomparator/ui/ProductListFragment.kt
+++ app/src/main/java/com/diyartaikenov/app/pricecomparator/ui/ProductListFragment.kt
@@ -1,16 +1,15 @@
 package com.diyartaikenov.app.pricecomparator.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
 
 import com.diyartaikenov.app.pricecomparator.BaseApplication
+import com.diyartaikenov.app.pricecomparator.R
 import com.diyartaikenov.app.pricecomparator.databinding.FragmentProductListBinding
+import com.diyartaikenov.app.pricecomparator.model.Product
 import com.diyartaikenov.app.pricecomparator.ui.adapter.ProductListAdapter
 import com.diyartaikenov.app.pricecomparator.ui.viewmodel.ProductViewModel
 import com.diyartaikenov.app.pricecomparator.ui.viewmodel.ProductViewModelFactory
@@ -26,12 +25,15 @@ class ProductListFragment: Fragment() {
     private var _bind: FragmentProductListBinding? = null
     private val bind get() = _bind!!
 
+    private lateinit var products: List<Product>
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _bind = FragmentProductListBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
         return bind.root
     }
 
@@ -52,6 +54,7 @@ class ProductListFragment: Fragment() {
 
         viewModel.products.observe(viewLifecycleOwner) { products ->
             adapter.submitList(products)
+            this.products = products
         }
 
         bind.apply {
@@ -68,5 +71,30 @@ class ProductListFragment: Fragment() {
     override fun onDestroyView() {
         _bind = null
         super.onDestroyView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.action_buttons_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.sort_by_protein_price -> {
+                bind.recyclerView.adapter
+            }
+            R.id.sort_by_protein_quantity -> {
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun List<Product>.sortByProteinPrice(): List<Product> {
+        val mutableList = this.toMutableList()
+        mutableList.sortWith(compareBy { it.proteinPrice })
+
+        return mutableList.toList()
     }
 }
